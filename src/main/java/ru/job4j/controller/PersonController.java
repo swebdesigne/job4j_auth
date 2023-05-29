5b7ecde96c9d2c3b5d2e31445811750dd7e7e377
Igor <swebdesigne@gmail.com>
2023-05-29 17:24:54 +0300

    Обработка исключений и Spring REST

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -1,13 +1,21 @@
 package ru.job4j.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.domain.Person;
 import ru.job4j.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,8 +23,10 @@ import java.util.Objects;
 @RequestMapping("/person")
 @AllArgsConstructor
 public class PersonController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(PersonController.class.getSimpleName());
     private final PersonService personService;
     private final BCryptPasswordEncoder encoder;
+    private final ObjectMapper objectMapper;
 
 
     @GetMapping("/")
@@ -25,12 +35,11 @@ public class PersonController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Person> findById(@PathVariable int id) {
-        var person = personService.findById(id);
-        return new ResponseEntity<>(
-                person.orElse(new Person()),
-                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
-        );
+    public Person findById(@PathVariable int id) {
+        return personService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Account is not found. Please, check requisites."
+                ));
     }
 
     @PostMapping("/")
@@ -64,8 +73,24 @@ public class PersonController {
         if (Objects.isNull(person.getLogin()) || Objects.isNull(person.getPassword())) {
             throw new NullPointerException("Username and password have not empty!!!");
         }
+        if (person.getPassword().length() < 5) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
         person.setPassword(encoder.encode(person.getPassword()));
         return new ResponseEntity<>(personService.save(person), HttpStatus.CREATED);
     }
 
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", e.getMessage());
+                put("type", e.getClass());
+            }
+        }));
+        LOGGER.error(e.getLocalizedMessage());
+    }
 }
---
+++ src/main/java/ru/job4j/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package ru.job4j.handlers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+@ControllerAdvice
+@AllArgsConstructor
+public class GlobalExceptionHandler {
+    private final static Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class.getSimpleName());
+    private final ObjectMapper objectMapper;
+
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", "Some of fields empty");
+                put("details", e.getMessage());
+            }
+        }));
+        LOGGER.error(e.getMessage());
+    }
+
+}
